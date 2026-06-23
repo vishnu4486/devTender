5815d8bcb35d3be0621d75261e4a71927dd978a6
vishnu biradar <biradarvishnu89@gmail.com>
2026-06-23 12:08:02 +0530

    exploding the methods

--- src/app.js
+++ src/app.js
@@ -4,17 +4,29 @@ const app = express();
 const PORT = 3000
 
 
-app.use("/home",(req, res) => {
- res.send("Hello from server!!!!!!!!!!!!")
+app.use("/home", (req, res) => {
+    res.send("Home Page !!!!!!!!!!!!")
 })
-app.use("/about",(req, res) => {
+app.use("/about", (req, res) => {
 
-    res.send("Hello from server!!!!!!!!!!!!")
+    res.send("About page !!!!!!!!!!!!")
 })
-app.use((req,res)=>{
+app.get("/user",(req,res)=>{
+    res.send({name:"vishnu",lastname:"Biradar"})
+})
+app.post("/user",(req,res)=>{
+    res.send("data save succsfully")
+})
+app.delete("/user",(req,res)=>{
 
-    res.send("This is default page")
+    res.send("Delete user succefully")
 })
+
+// app.use("/",(req, res) => {
+
+//     res.send("This is default page")
+// })
+
 
 app.listen(PORT, () => {
     console.log(`App ringing in ${PORT}`)
