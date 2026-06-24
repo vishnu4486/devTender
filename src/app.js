15221049f38ca886a94fdbce1b69e2cc7ef7b1af
vishnu biradar <biradarvishnu89@gmail.com>
2026-06-24 16:02:03 +0530

    Added error handle

--- src/app.js
+++ src/app.js
@@ -1,29 +1,26 @@
 const express = require("express");
-const {adminAuth,userAuth} =require("./middelwares/auth")
-
 const app = express();
 const PORT = 3000
-//its use middle ware
 
-app.use("/user/login",(req, res, next) => {
-    console.log("user login")
-    next();
-    //  res.send("Route Handler !!!!!!!!!!!!")
-})
-app.use("/user", userAuth,(req, res) => {
-    console.log("Route2 first")
-    res.send("Route 2 Handler !!!!!!!!!!!!")
-})
-app.use("/admin", adminAuth);
-app.get("/admin/getallData", (req, res) => {
 
-    res.send("fetch all data")
+app.get("/getUserDatat", (req, res) => {
+    // try {
+        throw new Error("sadjk jdsfs sajdfhasd ")
+        res.send("User data")
+    // } catch (err) {
+    //     res.status(500).send("Somthing went  wrong This catch error")
+    // }
 
 })
-app.delete("/admin/getallData", (req, res) => {
 
-    res.send("Delete the user")
+app.use((err, req, res, next) => {
+    console.error(err.message);
+    res.status(500).send("Something went wrong");
+});
+app.use("/", (req, res, next) => {
 
+         res.send("User data else ")
+    
 })
 app.listen(PORT, () => {
     console.log(`App ringing in ${PORT}`)
