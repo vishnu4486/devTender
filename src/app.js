0f5cc5050fdd2514fd3e7311515fabecb768bc5e
vishnu biradar <biradarvishnu89@gmail.com>
2026-06-25 22:56:08 +0530

    Formated code

--- src/app.js
+++ src/app.js
@@ -1,50 +1,53 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const { connectDatabse } = require("./config/database");
-const { validateSignUpData } = require("./utils/validation")
+const { validateSignUpData } = require("./utils/validation");
 const User = require("./models/user");
 const app = express();
-const PORT = 3000
+const PORT = 3000;
 
 app.use(express.json());
 app.post("/login", async (req, res) => {
-    try {
-
-        const { emailId, password } = req.body;
-        console.log("emailId, password", emailId, password)
-        const user = await User.findOne({ emailId })
-        if (!user) {
-            throw new Error("invalid credentials")
-        }
-        let data = await bcrypt.compare(password, user.password)
-        if (data) {
-            res.status(200).send("Login sucessfully")
-        } else {
-            throw new Error("invalid credentials")
-        }
-    } catch (error) {
-        res.status(400).json({
-            message: error.message,
-        });
+  try {
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId });
+    if (!user) {
+      throw new Error("invalid credentials");
     }
+    let data = await bcrypt.compare(password, user.password);
+    if (data) {
+      res.status(200).send("Login sucessfully");
+    } else {
+      throw new Error("invalid credentials");
+    }
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
 });
 app.post("/signup", async (req, res) => {
-    try {
-        validateSignUpData(req);
-        const { firstName, lastName, emailId, password } = req.body;
-        const passwordHas = await bcrypt.hash(password, 10)
-        const user = new User({ firstName, lastName, emailId, password: passwordHas });
+  try {
+    validateSignUpData(req);
+    const { firstName, lastName, emailId, password } = req.body;
+    const passwordHas = await bcrypt.hash(password, 10);
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHas,
+    });
 
-        await user.save();
-        res.status(201).json({
-            message: "User added successfully",
-            data: user,
-        });
-    } catch (error) {
-        res.status(400).json({
-            message: error.message,
-        });
-    }
+    await user.save();
+    res.status(201).json({
+      message: "User added successfully",
+      data: user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
 });
 // app.post("/signup", async (req, res) => {
 //     try {
@@ -65,86 +68,83 @@ app.post("/signup", async (req, res) => {
 //     }
 // });
 app.delete("/user", async (req, res) => {
-    try {
-        const { userId } = req.body;
-        const users = await User.findByIdAndDelete(userId);
-        if (users) {
-            res.send("Datat delete sucess fully")
-        } else {
-            res.status(404).send("given data is not found")
-        }
-    } catch (error) {
-        console.log(error)
-        res.status(500).send("Something went wrong")
+  try {
+    const { userId } = req.body;
+    const users = await User.findByIdAndDelete(userId);
+    if (users) {
+      res.send("Datat delete sucess fully");
+    } else {
+      res.status(404).send("given data is not found");
     }
-})
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Something went wrong");
+  }
+});
 app.put("/user/:userId", async (req, res) => {
-    try {
-        const _id = req.params?.userId
-        const { ...updateData } = req.body;
-        const ALLOW_UPDATE = ["age", "skills", "photoUrl", "gender", "about"]
+  try {
+    const _id = req.params?.userId;
+    const { ...updateData } = req.body;
+    const ALLOW_UPDATE = ["age", "skills", "photoUrl", "gender", "about"];
 
-        const updateKeys = Object.keys(updateData);
+    const updateKeys = Object.keys(updateData);
 
-        const UPDATE_FLAG = updateKeys.every(key => ALLOW_UPDATE.includes(key));
-        console.log("UPDATE_FLAG", UPDATE_FLAG)
-        if (!UPDATE_FLAG) {
-            return res.status(400).send("Updated not allowed")
-        }
-        const user = await User.findByIdAndUpdate(
-            _id,
-            updateData,
-            { returnDocument: "after", runValidators: true }
-        );
-
-        if (!user) {
-            return res.status(404).send("User not found");
-        }
-
-        res.send("Uodated user");
-    } catch (error) {
-        if (error.name === "ValidationError") {
-            return res.status(400).json({
-                message: "Validation failed",
-                error: error.message,
-            });
-        }
-        console.error(error);
-        res.status(500).send("Something went wrong");
+    const UPDATE_FLAG = updateKeys.every((key) => ALLOW_UPDATE.includes(key));
+    console.log("UPDATE_FLAG", UPDATE_FLAG);
+    if (!UPDATE_FLAG) {
+      return res.status(400).send("Updated not allowed");
     }
+    const user = await User.findByIdAndUpdate(_id, updateData, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    res.send("Uodated user");
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      return res.status(400).json({
+        message: "Validation failed",
+        error: error.message,
+      });
+    }
+    console.error(error);
+    res.status(500).send("Something went wrong");
+  }
 });
 
 app.post("/user", async (req, res) => {
-    try {
-        const { emailId } = req.body;
-        const users = await User.findOne({ emailId });
-        // const users = await User.find({ emailId });
-        if (users) {
-
-
-            res.json(users)
-        } else {
-            res.status(404).send("given data is not found")
-        }
-    } catch (error) {
-        res.status(500).send("Something went wrong");
+  try {
+    const { emailId } = req.body;
+    const users = await User.findOne({ emailId });
+    // const users = await User.find({ emailId });
+    if (users) {
+      res.json(users);
+    } else {
+      res.status(404).send("given data is not found");
     }
-})
-app.get("/feed", async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.json(users);
-    } catch (error) {
-        res.status(500).send("Something went wrong");
-    }
+  } catch (error) {
+    res.status(500).send("Something went wrong");
+  }
 });
-connectDatabse().then((res) => {
-    console.log("Database connected sucessfully")
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.json(users);
+  } catch (error) {
+    res.status(500).send("Something went wrong");
+  }
+});
+connectDatabse()
+  .then((res) => {
+    console.log("Database connected sucessfully");
     app.listen(PORT, () => {
-        console.log(`App ringing in ${PORT}`)
-    })
-}).catch((err) => {
-
-    console.log("Database not connected sucessfully")
-})
-
+      console.log(`App ringing in ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Database not connected sucessfully");
+  });
